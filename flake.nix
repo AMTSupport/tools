378ca9f37cfd5742ca3ca02c64615a183ecdee82
DaRacci <me@racci.dev>
2023-06-05 14:24:55 +1000

    chore: Add direnv and nix shell

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "dev-shell for tools";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system: let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+      in with pkgs; {
+        devShells.default = mkShell {
+          packages = [ pkgs.bashInteractive ];
+          buildInputs = [
+            openssl
+            pkg-config
+            (rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
+              extensions = [ "rust-src" ];
+              targets = [ "x86_64-unknown-linux-gnu" "x86_64-pc-windows-gnu" ];
+            }))
+          ];
+        };
+      });
+}
+
