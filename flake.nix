1fcce757cb454be70de007480659a0ece18e0dfe
DaRacci <me@racci.dev>
2023-06-19 15:40:47 +1000

    feat: Better flake

--- flake.nix
+++ flake.nix
@@ -2,28 +2,115 @@
   description = "dev-shell for tools";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    naersk = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+  outputs = { self, nixpkgs, flake-utils, naersk, fenix, ... }:
+    let
+#      cargoToml = builtins.fromToml (builtins.readFile ./Cargo.toml);
+#      name = cargoToml.package.name;
+    in
     flake-utils.lib.eachDefaultSystem (system: let
-        overlays = [ (import rust-overlay) ];
+        overlays = [ fenix.overlays.default ];
         pkgs = import nixpkgs { inherit system overlays; };
-      in with pkgs; {
-        devShells.default = mkShell {
+        lib = pkgs.lib;
+
+        toolchain = with fenix.packages.${system}; combine [
+          minimal.cargo
+          minimal.rustc
+          complete.rust-src
+          targets.x86_64-pc-windows-gnu.latest.rust-std
+          targets.x86_64-unknown-linux-gnu.latest.rust-std
+        ];
+
+        naersk-lib = naersk.lib.${system}.override {
+          cargo = toolchain;
+          rustc = toolchain;
+        };
+
+        buildPackage = target: { nativeBuildInputs ? [ ], ...}@args:
+          naersk-lib.buildPackage({
+              name = "tools";
+              src = ./.;
+              doCheck = false;
+              strictDeps = true;
+              release = false;
+            } // (lib.optionalAttrs (target != system) {
+              CARGO_BUILD_TARGET = target;
+            }) // args // {
+              nativeBuildInputs = with pkgs; [
+#                fenix.complete.rustfmt-preview
+              ] ++ nativeBuildInputs;
+            }
+          );
+
+#
+#        buildWindows = target: { nativeBuildInputs ? [ ], ...}@args:
+#          buildPackage "x86_64-pc-windows-gnu" {
+#            doCheck = false;#system == "x86_64-linux";
+#
+#            depsBuildBuild = with pkgs; [
+#              pkgsCross.mingwW64.stdenv.cc
+#              pkgsCross.mingwW64.windows.pthreads
+#            ];
+#
+#            nativeBuildInputs = lib.optional doCheck pkgs.wineWowPackages.stable;
+#
+#            CARGO_BUILD_TARGET = "x86_64-pc-windows-gnu";
+#            CARGO_TARGET_X86_64_PC_WINDOWS_GNU_RUNNER = pkgs.writeScript "wine-wrapper" ''
+#              # Without this, wine will error out when attempting to create the
+#              # prefix in the build's homeless shelter.
+#              export WINEPREFIX="$(mktemp -d)"
+#              exec wine64 $@
+#            '';
+#          };
+      in rec {
+        packages = {
+          # TODO :: Default run/build from system arch
+          default = buildPackage system { };
+
+          backup.x86_64-unknown-linux-gnu = buildPackage "x86_64-unkown-linux-gnu" {
+            nativeBuildInputs = with pkgs; [ openssl pkgsStatic.stdenv.cc mold ];
+            CARGO_BUILD_TARGET = "x86_64-unknown-linux-gnu";
+            CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_RUSTFLAGS = "-C link-arg=-fuse-ld=mold";
+          };
+
+          backup.x86_64-pc-windows-gnu = buildPackage "x86_64-pc-windows-gnu" {
+            doCheck = false;#system == "x86_64-linux";
+
+            depsBuildBuild = with pkgs; [
+              pkgsCross.mingwW64.stdenv.cc
+              pkgsCross.mingwW64.buildPackages.gcc
+              pkgsCross.mingwW64.windows.pthreads
+            ];
+
+#            nativeBuildInputs = lib.optional doCheck pkgs.wineWowPackages.stable;
+
+            CARGO_BUILD_TARGET = "x86_64-pc-windows-gnu";
+            CARGO_TARGET_X86_64_PC_WINDOWS_GNU_RUNNER = pkgs.writeScript "wine-wrapper" ''
+              # Without this, wine will error out when attempting to create the
+              # prefix in the build's homeless shelter.
+              export WINEPREFIX="$(mktemp -d)"
+              exec wine64 $@
+            '';
+          };
+        };
+
+
+        devShells.default = pkgs.mkShell {
           packages = [ pkgs.bashInteractive ];
-          buildInputs = [
-            glibc
-            gcc_multi
-            gcc
-            pkgsCross.mingwW64.buildPackages.gcc
-            mold
-            pkg-config
-            (rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
-              targets = [ "x86_64-unknown-linux-gnu" "x86_64-pc-windows-gnu" ];
-            }))
+          nativeBuildInputs = with pkgs; [
+            license-cli
+            cargo
           ];
         };
       });
