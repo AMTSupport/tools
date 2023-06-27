b98a3b924d2c1aa96e63a8bac3f87d4c239d61e3
DaRacci <me@racci.dev>
2023-06-27 17:26:38 +1000

    chore: License

--- crates/rest/src/hudu/web.rs
+++ crates/rest/src/hudu/web.rs
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2023 James Draycott <me@racci.dev>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 use crate::hudu::structs::company::{Companies, Company};
 use crate::hudu::structs::password::{Password, Passwords};
 use crate::hudu::{API_ENDPOINT, API_HEADER, COMPANIES_ENDPOINT, PASSWORDS_ENDPOINT};
