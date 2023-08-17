1d791fa30ce0c0627ed14fd93b32dfd52cdcd01b
DaRacci <me@racci.dev>
2023-08-17 19:00:51 +1000

    refactor(macros): cleanup and tests

---
+++ crates/macros/src/enums/common_fields.rs
@@ -0,0 +1,234 @@
+/*
+ * Copyright (C) 2023 James Draycott <me@racci.dev>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+use proc_macro2::{Ident, TokenStream};
+use quote::quote;
+use syn::spanned::Spanned;
+use syn::{Data, DataEnum, DeriveInput, Fields, Type};
+
+pub fn common_fields(input: DeriveInput) -> TokenStream {
+    let enum_name = &input.ident;
+
+    let variants = match input.data {
+        Data::Enum(ref item_enum) => &item_enum.variants,
+        ref other => return crate::error_input::<DataEnum, _>(input.span(), other),
+    };
+
+    // Extract the common field names and types from the enum variants
+    if variants.is_empty() {
+        return crate::error(input.span(), "Enum has no variants");
+    }
+
+    let mut global_fields = get_fields_from_variant(&variants[0]);
+    if global_fields.is_empty() {
+        return crate::error(variants[0].span(), "Enum variant has no fields.");
+    }
+
+    for variant in variants.iter().skip(1) {
+        let fields = get_fields_from_variant(variant);
+        if fields.is_empty() {
+            return crate::error(variant.span(), "Enum variant has no fields.");
+        }
+
+        // Will remove all fields that are not in the current variant
+        global_fields.retain(|(field_name, field_type)| {
+            fields.iter().any(|(other_field_name, other_field_type)| {
+                field_name == other_field_name && field_type == other_field_type
+            })
+        });
+    }
+
+    let common_fields = global_fields;
+
+    // Generate the conditional fields
+    let functions = common_fields.iter().map(|(field_name, field_type)| {
+        let mut string = field_name.to_string();
+        if string.starts_with("__self_") {
+            string.retain(|c| c.is_numeric());
+        };
+
+        let (function_name, field_index) = match string.parse::<usize>() {
+            Ok(index) => (Ident::new(&format!("field_{index}"), field_name.span()), Some(index)),
+            Err(_) => (field_name.clone(), None),
+        };
+
+        let enum_branches = variants.iter().map(|variant| {
+            let variant_name = &variant.ident;
+            match field_index {
+                Some(index) => {
+                    let varargs = (0..index).map(|_| quote! { _ }).collect::<Vec<_>>();
+                    quote! { #enum_name::#variant_name(#(#varargs,)* field, ..) => field }
+                }
+                None => quote! { #enum_name::#variant_name { #field_name, .. } => #field_name },
+            }
+        });
+
+        quote! {
+            #[automatically_derived]
+            pub const fn #function_name(&self) -> &#field_type {
+                match self {
+                    #(#enum_branches,)*
+                }
+            }
+        }
+    });
+
+    let output = quote! {
+        #[automatically_derived]
+        impl #enum_name {
+            #(#functions)*
+        }
+    };
+
+    // Convert the generated code back into tokens and return them
+    output.into()
+}
+
+// Helper function get get all fields from a enum variant
+fn get_fields_from_variant(variant: &syn::Variant) -> Vec<(Ident, &Type)> {
+    match &variant.fields {
+        Fields::Unit => vec![],
+        Fields::Named(fields) => fields
+            .named
+            .iter()
+            .map(|field| {
+                let field_name = field.ident.as_ref().unwrap().clone();
+                let field_type = &field.ty;
+                (field_name, field_type)
+            })
+            .collect::<Vec<(Ident, &Type)>>(),
+        Fields::Unnamed(fields) => fields
+            .unnamed
+            .iter()
+            .enumerate()
+            .map(|(index, field)| {
+                let field_name = Ident::new(&format!("__self_{}", index), field.span());
+                let field_type = &field.ty;
+                (field_name, field_type)
+            })
+            .collect::<Vec<(Ident, &Type)>>(),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use syn::parse_quote;
+
+    fn is_compile_error(input: DeriveInput) -> bool {
+        common_fields(input).to_string().contains("compile_error!")
+    }
+
+    #[test]
+    fn named_fields() {
+        let input: DeriveInput = parse_quote! {
+            enum Test {
+                A { a: u8, b: u8 },
+                B { a: u8, b: u8 },
+                C { a: u8, b: u8 },
+            }
+        };
+
+        let expected = quote! {
+            #[automatically_derived]
+            impl Test {
+                #[automatically_derived]
+                pub const fn a(&self) -> &u8 {
+                    match self {
+                        Test::A { a, .. } => a,
+                        Test::B { a, .. } => a,
+                        Test::C { a, .. } => a,
+                    }
+                }
+                #[automatically_derived]
+                pub const fn b(&self) -> &u8 {
+                    match self {
+                        Test::A { b, .. } => b,
+                        Test::B { b, .. } => b,
+                        Test::C { b, .. } => b,
+                    }
+                }
+            }
+        };
+
+        assert!(!is_compile_error(input.clone()));
+        assert_eq!(common_fields(input).to_string(), expected.to_string());
+    }
+
+    #[test]
+    fn unnamed_fields() {
+        let input: DeriveInput = parse_quote! {
+            enum Test {
+                A(u8, u8),
+                B(u8, u8),
+                C(u8, u8),
+            }
+        };
+
+        let expected = quote! {
+            #[automatically_derived]
+            impl Test {
+                #[automatically_derived]
+                pub const fn field_0(&self) -> &u8 {
+                    match self {
+                        Test::A(field, ..) => field,
+                        Test::B(field, ..) => field,
+                        Test::C(field, ..) => field,
+                    }
+                }
+                #[automatically_derived]
+                pub const fn field_1(&self) -> &u8 {
+                    match self {
+                        Test::A(_, field, ..) => field,
+                        Test::B(_, field, ..) => field,
+                        Test::C(_, field, ..) => field,
+                    }
+                }
+            }
+        };
+
+        assert!(!is_compile_error(input.clone()));
+        assert_eq!(common_fields(input).to_string(), expected.to_string());
+    }
+
+    #[test]
+    fn mixed_fields() {
+        let input: DeriveInput = parse_quote! {
+            enum Test {
+                A { a: u8, b: u8, c: u8 },
+                B { a: u8, c: u8 },
+                C { a: u8, b: u8 },
+            }
+        };
+
+        let expected = quote! {
+            #[automatically_derived]
+            impl Test {
+                #[automatically_derived]
+                pub const fn a(&self) -> &u8 {
+                    match self {
+                        Test::A { a, .. } => a,
+                        Test::B { a, .. } => a,
+                        Test::C { a, .. } => a,
+                    }
+                }
+            }
+        };
+
+        assert!(!is_compile_error(input.clone()));
+        assert_eq!(common_fields(input).to_string(), expected.to_string());
+    }
+}
--- crates/macros/src/lib.rs
+++ crates/macros/src/lib.rs
@@ -17,17 +17,32 @@
 #![feature(proc_macro_diagnostic)]
 #![feature(result_option_inspect)]
 #![feature(downcast_unchecked)]
+#![feature(type_name_of_val)]
 
-use proc_macro::Level::Error;
-use proc_macro::{Diagnostic, Span, TokenStream};
+mod enums;
+
+use proc_macro::TokenStream;
+use proc_macro2::TokenStream as TokenStream2;
 use quote::quote;
+use std::any::{type_name, type_name_of_val};
 use syn::parse::Parser;
 use syn::spanned::Spanned;
-use syn::{parse_macro_input, Data, DeriveInput, Field, Fields, Ident, Path, Type, TypePath};
+use syn::{parse_macro_input, Data, DeriveInput, Field, Fields, Ident, Path, TypePath};
 
-fn error(span: proc_macro2::Span, message: &str) -> TokenStream {
-    span.unwrap().error(message).emit();
-    syn::Error::new(span, message).into_compile_error().into()
+fn error(span: proc_macro2::Span, message: &str) -> TokenStream2 {
+    // span.unwrap().error(message).emit();
+    syn::Error::new(span, message).into_compile_error()
+}
+
+fn error_input<E, O>(span: proc_macro2::Span, other: O) -> TokenStream2 {
+    error(
+        span,
+        &format!(
+            "Derive macro can only be applied to {}, got {}",
+            type_name::<E>(),
+            type_name_of_val(&other),
+        ),
+    )
 }
 
 #[proc_macro_attribute]
@@ -36,7 +51,7 @@ pub fn runtime_cli(_args: TokenStream, input: TokenStream) -> TokenStream {
 
     let struct_data = match &mut input.data {
         Data::Struct(ref mut data) => data,
-        _ => return error(input.span(), "RuntimeCLI can only be derived for structs"),
+        _ => return error(input.span(), "RuntimeCLI can only be derived for structs").into(),
     };
 
     let struct_fields = match &mut struct_data.fields {
@@ -46,6 +61,7 @@ pub fn runtime_cli(_args: TokenStream, input: TokenStream) -> TokenStream {
                 pointer.span(),
                 "RuntimeCLI can only be derived for structs with named fields",
             )
+            .into()
         }
     };
 
@@ -58,7 +74,7 @@ pub fn runtime_cli(_args: TokenStream, input: TokenStream) -> TokenStream {
             .unwrap(),
     );
 
-    return quote! {
+    let expanded = quote! {
         use lib::cli::Flags;
         use clap::Parser;
 
@@ -71,8 +87,9 @@ pub fn runtime_cli(_args: TokenStream, input: TokenStream) -> TokenStream {
                 &self.flags
             }
         }
-    }
-    .into();
+    };
+
+    expanded.into()
 }
 
 #[proc_macro_attribute]
@@ -82,7 +99,7 @@ pub fn runtime(args: TokenStream, input: TokenStream) -> TokenStream {
 
     let struct_data = match &mut input.data {
         Data::Struct(data) => data,
-        _ => return error(input.span(), "Runtime can only be derived for structs"),
+        _ => return error(input.span(), "Runtime can only be derived for structs").into(),
     };
 
     let struct_fields = match &mut struct_data.fields {
@@ -92,6 +109,7 @@ pub fn runtime(args: TokenStream, input: TokenStream) -> TokenStream {
                 pointer.span(),
                 "Runtime can only be derived for structs with named fields",
             )
+            .into()
         }
     };
 
@@ -101,7 +119,7 @@ pub fn runtime(args: TokenStream, input: TokenStream) -> TokenStream {
         Field::parse_named.parse2(quote! { pub logger: tracing::dispatcher::DefaultGuard }),
     ]
     .into_iter()
-    .map(|f| f.map_err(|e| error(e.span(), "Failed to parse field")))
+    .map(|f| f.map_err(|e| error(e.span(), "Failed to parse field").into()))
     .collect::<Vec<Result<Field, TokenStream>>>();
 
     for field in new_fields {
@@ -113,7 +131,7 @@ pub fn runtime(args: TokenStream, input: TokenStream) -> TokenStream {
 
     let struct_ident = &input.ident;
 
-    return quote! {
+    let expanded = quote! {
         use lib::runtime::runtime::Runtime as RuntimeBase;
         use anyhow::{Result,Error};
 
@@ -144,18 +162,17 @@ pub fn runtime(args: TokenStream, input: TokenStream) -> TokenStream {
                 &mut self.errors
             }
         }
-    }
-    .into();
+    };
+
+    expanded.into()
 }
 
 #[proc_macro_derive(EnumVariants)]
 pub fn enum_variants(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
-    let name = &input.ident;
-    let variants = match input.data {
-        Data::Enum(ref data) => &data.variants,
-        _ => panic!("EnumVariants can only be derived for enums"),
-    };
+
+    enums::variants::variants(input).into()
+}
 
     let variant_names = variants.iter().map(|variant| &variant.ident).collect::<Vec<_>>();
 
@@ -178,7 +195,7 @@ pub fn delegate_trait(input: TokenStream) -> TokenStream {
     // Check if the input is an enum
     let item_enum = match &input.data {
         Data::Enum(item_enum) => item_enum,
-        _ => return error(input.span(), "Delegate can only be derived for enums"),
+        _ => return error(input.span(), "Delegate can only be derived for enums").into(),
     };
 
     // Retrieve the name of the enum
@@ -211,7 +228,7 @@ pub fn delegate_trait(input: TokenStream) -> TokenStream {
         let ident = &variant.ident;
         let attr = match variant.attrs.iter().find(|a| a.path().is_ident("delegate")) {
             Some(attr) => attr,
-            None => return error(variant.span(), "Missing `delegate` attribute on variant"),
+            None => return error(variant.span(), "Missing `delegate` attribute on variant").into(),
         };
 
         let nested = attr.parse_nested_meta(|meta| {
@@ -259,312 +276,6 @@ pub fn delegate_trait(input: TokenStream) -> TokenStream {
 
 #[proc_macro_derive(CommonFields)]
 pub fn conditional_fields_macro(input: TokenStream) -> TokenStream {
-    fn error(span: Span) -> TokenStream {
-        let err = "Derive macro CommonFields requires all enums be variants with at least one field.";
-        Diagnostic::spanned(span, Error, err).emit();
-        TokenStream::new()
-    }
-
-    // Parse the input tokens into a syntax tree
     let input = parse_macro_input!(input as DeriveInput);
-
-    // Extract the enum name
-    let enum_name = input.ident;
-
-    let (item_enum, variants) = match &input.data {
-        Data::Enum(item_enum) => (item_enum, &item_enum.variants),
-        _ => return error(enum_name.span().unwrap()),
-    };
-
-    // Extract the common field names and types from the enum variants
-    if variants.is_empty() {
-        return error(item_enum.enum_token.span().unwrap());
-    }
-
-    let mut global_fields = get_fields_from_variant(&variants[0]);
-    if global_fields.is_empty() {
-        return error(variants[0].span().unwrap());
-    }
-
-    for variant in variants.iter().skip(1) {
-        let fields = get_fields_from_variant(variant);
-        if fields.is_empty() {
-            return error(variant.span().unwrap());
-        }
-
-        // Will remove all fields that are not in the current variant
-        global_fields.retain(|(field_name, field_type)| {
-            fields.iter().any(|(other_field_name, other_field_type)| {
-                field_name == other_field_name && field_type == other_field_type
-            })
-        });
-    }
-
-    let common_fields = global_fields;
-
-    // Generate the conditional fields
-    let functions = common_fields.iter().map(|(field_name, field_type)| {
-        let mut string = field_name.to_string();
-        if string.starts_with("__self_") {
-            string.retain(|c| c.is_numeric());
-        };
-
-        let (function_name, field_index) = match string.parse::<usize>() {
-            Ok(index) => (Ident::new(&format!("field_{index}"), field_name.span()), Some(index)),
-            Err(_) => (field_name.clone(), None),
-        };
-
-        let enum_branches = variants.iter().map(|variant| {
-            let variant_name = &variant.ident;
-            match field_index {
-                Some(index) => {
-                    let varargs = (0..index).map(|_| quote! { _ }).collect::<Vec<_>>();
-                    quote! { #enum_name::#variant_name(#(#varargs,)* field, ..) => field }
-                }
-                None => quote! { #enum_name::#variant_name { #field_name, .. } => #field_name },
-            }
-        });
-
-        quote! {
-            #[automatically_derived]
-            pub const fn #function_name(&self) -> &#field_type {
-                match self {
-                    #(#enum_branches,)*
-                }
-            }
-        }
-    });
-
-    let output = quote! {
-        #[automatically_derived]
-        impl #enum_name {
-            #(#functions)*
-        }
-    };
-
-    // Convert the generated code back into tokens and return them
-    output.into()
+    enums::common_fields::common_fields(input).into()
 }
-
-// #[proc_macro_derive(FromCommand, attributes(command))]
-// pub fn from_command(input: TokenStream) -> TokenStream {
-//     let input = parse_macro_input!(input as DeriveInput);
-//     let struct_name = &input.ident;
-//     let struct_data = match input.data {
-//         Data::Struct(data) => data,
-//         _ => panic!("FromCommand can only be derived for structs"),
-//     };
-//
-//     let fields = match struct_data.fields {
-//         Fields::Named(fields) => fields,
-//         _ => {
-//             return syn::Error::new(
-//                 struct_name.span(),
-//                 "FromCommand can only be derived for structs with named fields",
-//             )
-//             .to_compile_error()
-//             .into()
-//         }
-//     };
-//
-//     let attributes = &input.attrs;
-//     let binary: PathBuf = parse_attribute_path(&attributes, "binary");
-//     let args: Vec<String> = parse_attribute_vec(&attributes, "args");
-//     let env: Vec<String> = parse_attribute_tuples(&attributes, "env");
-// }
-//
-// // Helper functions to parse attribute values
-//
-// fn parse_attribute_path<T>(attributes: &[syn::Attribute], name: &str) -> Option<T>
-// where
-//     T: syn::parse::Parse,
-// {
-//     for attr in attributes {
-//         let meta_list = match &attr.meta {
-//             Meta::List(m) => m,
-//             _ => continue,
-//         };
-//
-//         let result: Option<T> = None;
-//         meta_list.parse_nested_meta(|m| match m {
-//             Meta::NameValue(value) => {
-//                 if result.is_some() {
-//                     return Ok(()); // continue
-//                 }
-//
-//                 if !value.paths.is_ident(name) {
-//                     return Ok(()); // continue
-//                 }
-//
-//                 if let syn::Expr::Path(paths) = value.value {
-//                     let parsed = paths.parse::<T>();
-//                     parsed.map_err(|e| {
-//                         Diagnostic::spanned(value.span(), Error, e.to_string()).emit();
-//                     });
-//                 }
-//
-//                 Ok(())
-//             }
-//             _ => Ok(()),
-//         })?;
-//
-//         result;
-//     }
-//
-//     None
-// }
-//
-// fn parse_attribute_vec(attributes: &[syn::Attribute], name: &str) -> Vec<String> {
-//     for attr in attributes {
-//         if let Ok(syn::Meta::List(meta_list)) = attr.parse_meta() {
-//             if meta_list.paths.is_ident(name) {
-//                 if let Some(syn::NestedMeta::Lit(syn::Lit::Str(lit))) = meta_list.nested.first() {
-//                     return lit.value().split(',').map(|s| s.trim().to_string()).collect();
-//                 }
-//             }
-//         }
-//     }
-//
-//     Vec::new()
-// }
-//
-// fn parse_attribute_tuples(attributes: &[syn::Attribute], name: &str) -> Vec<(String, String)> {
-//     for attr in attributes {
-//         if let Ok(syn::Meta::List(meta_list)) = attr.parse_meta() {
-//             if meta_list.paths.is_ident(name) {
-//                 return meta_list
-//                     .nested
-//                     .iter()
-//                     .filter_map(|meta| {
-//                         if let syn::NestedMeta::Meta(syn::Meta::NameValue(value)) = meta {
-//                             if let (syn::Lit::Str(key), syn::Lit::Str(val)) =
-//                                 (&value.paths.segments.last().unwrap().ident, &value.lit)
-//                             {
-//                                 return Some((key.to_string(), val.value()));
-//                             }
-//                         }
-//                         None
-//                     })
-//                     .collect();
-//             }
-//         }
-//     }
-//
-//     Vec::new()
-// }
-
-// #[proc_macro_derive(Pathed, attributes(pathed))]
-// pub fn pathed_macro(input: TokenStream) -> TokenStream {
-//     let input = parse_macro_input!(input as DeriveInput);
-//
-//     let struct_name = input.ident;
-//     let name = input
-//         .attrs
-//         .iter()
-//         .filter_map(|a| a.meta.require_list().ok())
-//         .find_map(|attr| match attr.meta {
-//             Meta::NameValue(ref meta) if meta.paths.is_ident("name") => Some(&meta.paths),
-//             _ => None,
-//         })
-//         .expect("expected `name` attribute");
-//     let type_name = input
-//         .attrs
-//         .iter()
-//         .find_map(|attr| match attr.meta {
-//             Meta::Path(ref meta) if meta.is_ident("type") => Some(meta),
-//             _ => None,
-//         })
-//         .expect("expected `type` attribute");
-//
-//     let output = quote! {
-//         impl Pathed<#type_name> for #struct_name {
-//             const NAME: &'static str = stringify!(#name);
-//
-//             fn unique_dir(&self, ref from: Self::TYPE) -> PathBuf {
-//                 Self::base_dir(from).join(self)
-//             }
-//         }
-//     };
-//
-//     output.into()
-// }
-//
-// // Helper function to extract attribute values
-// fn get_attribute_value(attrs: &[Attribute], attr_name: &str) -> Option<String> {
-//     for attr in attrs {
-//         let segments = &attr.paths().segments;
-//         for segment in segments {
-//             let attr_name = segment.ident.to_string();
-//             if attr_name != attr_name {
-//                 continue;
-//             }
-//
-//             let t = attr.to_token_stream().into_iter().collect::<Vec<TokenTree>>();
-//             let stream = if let TokenTree::Group(group) = &t[0] {
-//                 group.stream()
-//             } else {
-//                 unimplemented!()
-//             };
-//
-//
-//         }
-//
-//         let attr = attr.to_owned();
-//         let meta = match attr.meta {
-//             Meta::List(meta) => meta,
-//             _ => continue,
-//         };
-//
-//         let mut value: Option<String> = None;
-//         meta.parse_nested_meta(|meta| {
-//             if meta.paths.is_ident(attr_name) {
-//                 let unparsed = meta.value().expect("Failed to get meta value");
-//                 let lit = syn::Lit::parse(unparsed).expect("Failed to parse to literal");
-//                 let val = match lit {
-//                     syn::Lit::Str(lit) => lit.value(),
-//                     _ => return Err(meta.error("expected string literal")),
-//                 };
-//
-//                 value = Some(val);
-//                 Ok(())
-//             } else {
-//                 Ok(())
-//             }
-//         })
-//         .expect("failed to parse attribute");
-//
-//         if let Some(value) = value {
-//             return Some(value);
-//         }
-//     }
-//
-//     None
-// }
-
-// Helper function get get all fields from a enum variant
-fn get_fields_from_variant(variant: &syn::Variant) -> Vec<(Ident, &Type)> {
-    match &variant.fields {
-        Fields::Unit => vec![],
-        Fields::Named(fields) => fields
-            .named
-            .iter()
-            .map(|field| {
-                let field_name = field.ident.as_ref().unwrap().clone();
-                let field_type = &field.ty;
-                (field_name, field_type)
-            })
-            .collect::<Vec<(Ident, &Type)>>(),
-        Fields::Unnamed(fields) => fields
-            .unnamed
-            .iter()
-            .enumerate()
-            .map(|(index, field)| {
-                let field_name = Ident::new(&format!("__self_{}", index), field.span());
-                let field_type = &field.ty;
-                (field_name, field_type)
-            })
-            .collect::<Vec<(Ident, &Type)>>(),
-    }
-}
-
-// #[proc_macro_derive(CommandFiller)]
