602dba49bfdc4cb33006b270c037b807e2ccb7e6
DaRacci <me@racci.dev>
2023-09-18 13:19:20 +1000

    fix(memorable-pass): invalid error

--- crates/memorable-pass/src/config/asset.rs
+++ crates/memorable-pass/src/config/asset.rs
@@ -31,16 +31,9 @@ fn get_words() -> HashMap<usize, Vec<String>> {
 
     let asset_file =
         Asset::get("words.json").context("Find words.json asset file.").expect("Failed to find words.json asset file.");
-    let words_vec =
-        serde_json::from_slice::<Vec<Vec<String>>>(&asset_file.data).context("Parse words.json asset into Vec");
     let words_map = serde_json::from_slice::<HashMap<usize, Vec<String>>>(&asset_file.data)
         .context("Parse words.json asset into Map")
         .expect("Failed to parse words.json asset into Map");
-
-    match words_vec {
-        Ok(_) => info!("Parsed as Vec<Vec<String>>"),
-        Err(e) => error!("No vec parse :( {e:?}"),
-    }
 
     debug!("Loaded words in {}ms", start.elapsed().as_millis());
 
