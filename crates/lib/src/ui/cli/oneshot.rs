75d66daf84dc3fae0f63d8f01c5357cca4bc54d5
DaRacci <me@racci.dev>
2024-02-29 13:17:25 +1100

    fix: implement replhandler for oneshothandler

--- crates/lib/src/ui/cli/oneshot.rs
+++ crates/lib/src/ui/cli/oneshot.rs
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023 James Draycott <me@racci.dev>
+ * Copyright (C) 2024. James Draycott me@racci.dev
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -7,11 +7,20 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * along with this program. If not, see https://www.gnu.org/licenses/.
  */
 
 crate::handler!(pub Oneshot);
+
+#[cfg(feature = "ui-repl")]
+impl<T: OneshotHandler> crate::ui::cli::repl::ReplHandler for T {
+    type Action = <Self as OneshotHandler>::Action;
+
+    async fn handle(&mut self, command: Self::Action, flags: &_CommonFlags) -> _CliResult<()> {
+        self.handle(command, flags).await
+    }
+}
