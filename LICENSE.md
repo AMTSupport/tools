892467d8ec356a9ae9e295f50d8c376e686fd5b2
DaRacci <me@racci.dev>
2024-04-04 17:52:12 +1100

    chore(deps): Update deps

--- LICENSE.md
+++ LICENSE.md
@@ -1,4 +1,4 @@
-Copyright (C) 2023 James Draycott <me@racci.dev>
+Copyright (C) 2023-2024 James Draycott <me@racci.dev>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -6,8 +6,8 @@ the Free Software Foundation, version 3.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
+along with this program. If not, see <https://www.gnu.org/licenses/>.
--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
       "amt.cachix.org-1:KiJsXTfC7rGJc4DmNlLA56caUUWuc8YsOfzpPgredJI="
       "racci.cachix.org-1:Kl4opLxvTV9c77DpoKjUOMLDbCv6wy3GVHWxB384gxg="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-     ];
+    ];
   };
 
   inputs = {
@@ -32,215 +32,216 @@
 
     systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
 
-    perSystem = { config, system, pkgs, lib, ... }: let
-      rustToolchain = let fenixPkgs = inputs.fenix.packages.${system}; in fenixPkgs.combine [
-        fenixPkgs.complete.cargo
-        fenixPkgs.complete.rustc
-        fenixPkgs.complete.rust-src
-        fenixPkgs.complete.rust-analyzer
-        fenixPkgs.complete.clippy
-        fenixPkgs.complete.rustfmt
-      ];
+    perSystem = { config, system, pkgs, lib, ... }:
+      let
+        rustToolchain = let fenixPkgs = inputs.fenix.packages.${system}; in fenixPkgs.combine [
+          fenixPkgs.complete.cargo
+          fenixPkgs.complete.rustc
+          fenixPkgs.complete.rust-src
+          fenixPkgs.complete.rust-analyzer
+          fenixPkgs.complete.clippy
+          fenixPkgs.complete.rustfmt
+        ];
 
-      useMold = isNative: pkgs: envTarget: lib.mkIf (isNative && pkgs.stdenv.targetPlatform.isLinux) (let
-        clang = "${pkgs.clang}/bin/${pkgs.clang.targetPrefix}clang";
-      in {
-        "CC" = clang;
-        "CC_${envTarget}" = clang;
-        "CARGO_TARGET_${envTarget}_LINKER" = clang;
-        "CARGO_TARGET_${envTarget}_RUSTFLAGS" = "-C link-arg=-fuse-ld=${lib.getExe pkgs.mold}";
-      });
-    in rec {
-      pre-commit.settings.hooks = {
-        actionlint.enable = true;
+        useMold = isNative: pkgs: envTarget: lib.mkIf (isNative && pkgs.stdenv.targetPlatform.isLinux) (
+          let
+            clang = "${pkgs.clang}/bin/${pkgs.clang.targetPrefix}clang";
+          in
+          {
+            "CC" = clang;
+#            "CC_${envTarget}" = clang;
+            "CARGO_TARGET_${envTarget}_LINKER" = clang;
+            "CARGO_TARGET_${envTarget}_RUSTFLAGS" = "-C link-arg=-fuse-ld=${lib.getExe pkgs.mold}";
+          }
+        );
+      in
+      rec {
+        pre-commit.settings.hooks = {
+          actionlint.enable = true;
 
-        # Rust specific hooks
-        cargo-check = {
-          enable = true;
-          package = rustToolchain;
-          packageOverrides = {
-            cargo = rustToolchain;
-          };
-        };
-        clippy = {
-          enable = true;
-          packageOverrides = {
-            cargo = rustToolchain;
-            clippy = rustToolchain;
-          };
-        };
-        rustfmt = {
-          enable = true;
-          package = rustToolchain;
-          packageOverrides = {
-            cargo = rustToolchain;
-            rustfmt = rustToolchain;
-          };
-        };
+          # Rust specific hooks
+          cargo-check.enable = true;
+          clippy.enable = true;
+          rustfmt.enable = true;
 
-        check-case-conflicts.enable = true;
-        check-docstring-first.enable = true;
-        check-toml.enable = true;
-        check-vcs-permalinks.enable = true;
-        check-yaml.enable = true;
-        deadnix.enable = true;
-        detect-private-keys.enable = true;
-        editorconfig-checker.enable = true;
-        end-of-file-fixer.enable = true;
-        markdownlint.enable = true;
-        mdl.enable = true;
-        mdsh.enable = true;
-#        mixed-line-ending.enable = true;
-        nil.enable = true;
-        nixpkgs-fmt.enable = true;
-        statix.enable = true;
-        tagref.enable = true;
-        typos.enable = true;
-      };
+          check-case-conflicts.enable = true;
+          check-docstring-first.enable = true;
+          check-toml.enable = true;
+          check-vcs-permalinks.enable = true;
+          check-yaml.enable = true;
+          deadnix.enable = true;
+          detect-private-keys.enable = true;
+          editorconfig-checker.enable = true;
+          end-of-file-fixer.enable = true;
+          markdownlint.enable = true;
+          mdl.enable = true;
+          mdsh.enable = true;
+          #        mixed-line-ending.enable = true;
+          nil.enable = true;
+          nixpkgs-fmt.enable = true;
+          statix.enable = true;
+          tagref.enable = true;
 
-      devenv.shells.default = rec {
-        difftastic.enable = true;
-
-        languages.rust = {
-          enable = true;
-          channel = "nightly";
-          # Don't use any components here because we need a combined toolchain
-          # so that rust-rover is happy. :)
-          components = [ ];
+          typos.enable = true;
         };
 
-        packages = with pkgs; [
-          openssl
-          clang
-          mold
+        devenv.shells.default = rec {
+          difftastic.enable = true;
 
-          pkg-config
-          rustToolchain
+          languages.rust = {
+            enable = true;
+            channel = "nightly";
+            # Don't use any components here because we need a combined toolchain
+            # so that rust-rover is happy. :)
+            components = [ ];
 
-          act
-          hyperfine
-          cocogitto
-          cargo-udeps
-          cargo-audit
-          cargo-expand
-          cargo-nextest
-          cargo-cranky
-          cargo-edit
-        ] ++ config.pre-commit.settings.enabledPackages;
-
-        env = useMold true pkgs "X86_64_UNKNOWN_LINUX_GNU" // {
-          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (with pkgs; [
-            openssl
-            clang
-            mold
-          ]);
-        };
-      };
-
-      nci = rec {
-        toolchains = {
-          build = rustToolchain;
-          shell = devenv.shells.default;
-        };
-
-        projects.tools = {
-          path = self;
-          export = true;
-
-          profiles = {
-            dev = { };
-            release = {
-              runTests = true;
+            toolchain = {
+              cargo = rustToolchain;
+              rustfmt = rustToolchain;
+              clippy = rustToolchain;
             };
           };
 
-          targets = lib.trivial.pipe [
-            "unknown-linux-gnu"
-            "pc-windows-gnu"
-#            "apple-darwin"
-          ] [
-            (map (target: [
-              "x86_64-${target}"
-            ] ++ lib.optionals (target != "pc-windows-gnu") [ "aarch64-${target}" ]))
-            lib.flatten
-            (map (target: let
-              envTarget = builtins.replaceStrings [ "-" ] [ "_" ] (lib.toUpper target);
-              nixTarget = builtins.replaceStrings [ "-gnu" "-pc" "-unknown" "-apple" ] [ "" "" "" "" ] target;
-            in lib.nameValuePair target rec {
-              # Default if native
-              default = system == nixTarget;
-              # No dev profile for non-native targets
-              profiles = if default then (builtins.attrNames projects.tools.profiles) else [ "release" ];
-              # Only run tests on native target
-              # profiles.release.runTests = default;
+          packages = with pkgs; [
+            openssl
+            clang
+            mold
 
-              depsDrvConfig = let
-                crossPackages = if default then pkgs
-                  else if nixTarget == "x86_64-linux" then pkgs.pkgsCross.gnu64
-                  else if nixTarget == "x86_64-windows" then pkgs.pkgsCross.mingwW64
-                  else if nixTarget == "aarch64-linux" then pkgs.pkgsCross.aarch64-multiplatform
-                  else pkgs.pkgsCross.${nixTarget};
-              in {
-                deps.stdenv = crossPackages.clangStdenv;
+            pkg-config
+            rustToolchain
 
-                mkDerivation = {
-                  # Make sure some windows build dependencies are available.
-                  depsBuildBuild = lib.optionals crossPackages.targetPlatform.isWindows (with crossPackages; [
-                    stdenv.cc
-                    windows.mingw_w64_pthreads
-                    windows.pthreads
-                  ]);
+            act
+            hyperfine
+            cocogitto
+            cargo-udeps
+            cargo-audit
+            cargo-deny
+            cargo-expand
+            cargo-nextest
+            cargo-cranky
+            cargo-edit
+          ] ++ config.pre-commit.settings.enabledPackages;
 
-                  # Common build inputs
-                  buildInputs = [ pkgs.openssl ];
-                  nativeBuildInputs = [ pkgs.pkg-config ];
-                };
+          env = useMold true pkgs "X86_64_UNKNOWN_LINUX_GNU" // {
+            LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (with pkgs; [
+              openssl
+              clang
+              mold
+            ]);
+          };
+        };
 
-                # Use mold linker for linux targets
-                env = useMold default pkgs envTarget;
+        nci = rec {
+          toolchains = {
+            build = rustToolchain;
+            shell = devenv.shells.default;
+          };
+
+          projects.tools = {
+            path = self;
+            export = true;
+
+            profiles = {
+              dev = { };
+              release = {
+                runTests = true;
               };
-            }))
-            builtins.listToAttrs
-          ];
-        };
-      };
+            };
 
-      # Create a symlink to all binaries for each crate
-      # Create a symlink to all binaries for each target
-      packages = {
-        all = pkgs.symlinkJoin {
-          name = "all";
-          description = "Compile all crates for all targets";
-          paths = lib.trivial.pipe config.nci.outputs [
-            (lib.filterAttrs (name: _: name != "tools"))
-            lib.attrValues
-            (builtins.map (crate: lib.attrValues crate.allTargets))
-            lib.flatten
-            (builtins.map (target: target.packages.release))
-          ];
+            targets = lib.trivial.pipe [
+              "unknown-linux-gnu"
+              "pc-windows-gnu"
+              #            "apple-darwin"
+            ] [
+              (map (target: [
+                "x86_64-${target}"
+              ] ++ lib.optionals (target != "pc-windows-gnu") [ "aarch64-${target}" ]))
+              lib.flatten
+              (map (target:
+                let
+                  envTarget = builtins.replaceStrings [ "-" ] [ "_" ] (lib.toUpper target);
+                  nixTarget = builtins.replaceStrings [ "-gnu" "-pc" "-unknown" "-apple" ] [ "" "" "" "" ] target;
+                in
+                lib.nameValuePair target rec {
+                  # Default if native
+                  default = system == nixTarget;
+                  # No dev profile for non-native targets
+                  profiles = if default then (builtins.attrNames projects.tools.profiles) else [ "release" ];
+                  # Only run tests on native target
+                  # profiles.release.runTests = default;
+
+                  depsDrvConfig =
+                    let
+                      crossPackages =
+                        if default then pkgs
+                        else if nixTarget == "x86_64-linux" then pkgs.pkgsCross.gnu64
+                        else if nixTarget == "x86_64-windows" then pkgs.pkgsCross.mingwW64
+                        else if nixTarget == "aarch64-linux" then pkgs.pkgsCross.aarch64-multiplatform
+                        else pkgs.pkgsCross.${nixTarget};
+                    in
+                    {
+                      deps.stdenv = crossPackages.clangStdenv;
+
+                      mkDerivation = {
+                        # Make sure some windows build dependencies are available.
+                        depsBuildBuild = lib.optionals crossPackages.targetPlatform.isWindows (with crossPackages; [
+                          stdenv.cc
+                          windows.mingw_w64_pthreads
+                          windows.pthreads
+                        ]);
+
+                        # Common build inputs
+                        buildInputs = [ pkgs.openssl ];
+                        nativeBuildInputs = [ pkgs.pkg-config ];
+                      };
+
+                      # Use mold linker for linux targets
+                      env = useMold default pkgs envTarget;
+                    };
+                }))
+              builtins.listToAttrs
+            ];
+          };
         };
 
-        allNative = pkgs.symlinkJoin {
-          name = "allNative";
-          description = "Compile all crates for the native target";
-          paths = lib.trivial.pipe config.nci.outputs [
-            (lib.filterAttrs (name: _: name != "tools"))
-            lib.attrValues
-            (builtins.map (crate: crate.packages.release))
-          ];
-        };
-
-        allTargets = lib.mapAttrs (name: crate: {
+        # Create a symlink to all binaries for each crate
+        # Create a symlink to all binaries for each target
+        packages = {
           all = pkgs.symlinkJoin {
-            inherit name;
-            description = "Compile all targets for ${name}";
-            paths = lib.trivial.pipe crate.allTargets [
+            name = "all";
+            description = "Compile all crates for all targets";
+            paths = lib.trivial.pipe config.nci.outputs [
+              (lib.filterAttrs (name: _: name != "tools"))
               lib.attrValues
+              (builtins.map (crate: lib.attrValues crate.allTargets))
+              lib.flatten
               (builtins.map (target: target.packages.release))
             ];
           };
-        }) config.nci.outputs;
+
+          allNative = pkgs.symlinkJoin {
+            name = "allNative";
+            description = "Compile all crates for the native target";
+            paths = lib.trivial.pipe config.nci.outputs [
+              (lib.filterAttrs (name: _: name != "tools"))
+              lib.attrValues
+              (builtins.map (crate: crate.packages.release))
+            ];
+          };
+
+          allTargets = lib.mapAttrs
+            (name: crate: {
+              all = pkgs.symlinkJoin {
+                inherit name;
+                description = "Compile all targets for ${name}";
+                paths = lib.trivial.pipe crate.allTargets [
+                  lib.attrValues
+                  (builtins.map (target: target.packages.release))
+                ];
+              };
+            })
+            config.nci.outputs;
+        };
       };
-    };
   };
 }
