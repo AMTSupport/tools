d8b773014125d43b515fb44f80cc8c1b440c6318
DaRacci <me@racci.dev>
2024-11-20 05:55:17 +0000

    fix(backup): failing test

--- crates/backup/tests/config/rules/autoprune.rs
+++ crates/backup/tests/config/rules/autoprune.rs
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023 James Draycott <me@racci.dev>
+ * Copyright (C) 2023-2024. James Draycott me@racci.dev
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -7,11 +7,11 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * along with this program. If not, see https://www.gnu.org/licenses/.
  */
 
 use assert_fs::prelude::{FileTouch, PathChild};
@@ -74,7 +74,7 @@ fn tag_tag() {
     file.touch().expect("Failed to create file");
 
     let file = Tag::tag(file.path());
-    assert_matches!(file.file_name(), Some(name) if name == "Hourly-test.txt");
+    assert_matches!(file.file_name(), Some(name) if name == "Hourly-Daily-Weekly-Monthly-Yearly-test.txt");
 }
 
 #[test_log::test(test)]
